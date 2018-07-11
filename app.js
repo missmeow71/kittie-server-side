89a5fbdce80a27f891783ec1cd44703a55619d33
missmeow71 <sensuousdesigner@gmail.com>
2018-07-10 21:17:37 -0600

    added cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 
