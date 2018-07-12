511554bc42bbd468e412d04f7a55ecda5c6c0412
missmeow71 <sensuousdesigner@gmail.com>
2018-07-12 15:58:59 -0600

    changed placeholder size

--- seeds/01_kittie.js
+++ seeds/01_kittie.js
@@ -4,21 +4,21 @@ exports.seed = function(knex, Promise) {
     .then(function () {
          return knex('kittie').insert([{
         id: 1,
-        imgUrl: 'http://via.placeholder.com/250x250',
+        imgUrl: 'http://via.placeholder.com/500x500',
         catName: 'Meow Meow',
         comment: 'Pose purrfectly to show my beauty milk the cow so jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed jumps off balcony gives owner dead mouse at present then poops in litter box snatches yarn ',
         rating: '10'
 
         }, {
         id: 2,
-        imgUrl: 'http://via.placeholder.com/250x250',
+        imgUrl: 'http://via.placeholder.com/500x500',
         catName: 'Meow Meow',
         comment: 'Pose purrfectly to show my beauty milk the cow so jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed jumps off balcony gives owner dead mouse at present then poops in litter box snatches yarn ',
         rating: '10'
 
       }, {
         id: 3,
-        imgUrl: 'http://via.placeholder.com/250x250',
+        imgUrl: 'http://via.placeholder.com/500x500',
         catName: 'Meow Meow',
         comment: 'Pose purrfectly to show my beauty milk the cow so jump launch to pounce upon little yarn mouse, bare fangs at toy run hide in litter box until treats are fed jumps off balcony gives owner dead mouse at present then poops in litter box snatches yarn ',
         rating: '10'
